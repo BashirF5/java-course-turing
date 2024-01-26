fad1cec264dbdc3d5813d7b97b191cff6267d118
BashirF5 <besirhacili@gmail.com>
2024-01-26 19:59:56 +0400

    init java-course module

---
+++ src/main/java/az/edu/turing/lesson08/TaskFor08.java
@@ -0,0 +1,17 @@
+package az.edu.turing.lesson08;
+
+import java.util.Scanner;
+
+public class TaskFor08 {
+    public static void main(String[] args) {
+        System.out.println("Enter the number that you want to find the divisors of");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        if (a>10){
+            int[] array = new int[10];
+        }
+        if (a>1000){
+            int[] array = new int[1000];
+        }
+    }
+}
