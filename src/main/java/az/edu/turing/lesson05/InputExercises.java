0d3f4204f6d072e95daa827280e8e33b04f23a01
BashirF5 <besirhacili@gmail.com>
2024-01-26 15:10:56 +0400

    init java-course module

---
+++ src/main/java/az/edu/turing/lesson05/InputExercises.java
@@ -0,0 +1,11 @@
+package az.edu.turing.lesson05;
+
+import java.util.Scanner;
+public class InputExercises {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num1 = scanner.nextInt();
+    }
+}
+
