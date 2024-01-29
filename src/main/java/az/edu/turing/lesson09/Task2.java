6ee10e018a4ceed55b000fbfda87ab3afe11351d
BashirF5 <besirhacili@gmail.com>
2024-01-29 19:40:46 +0400

    init java-course module

---
+++ src/main/java/az/edu/turing/lesson09/Task2.java
@@ -0,0 +1,15 @@
+package az.edu.turing.lesson09;
+
+import java.lang.Math;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int a, y, z, r, x, s;
+        a = 5;
+        y = 4 * a;
+        z = a * a;
+        r = 2;
+        x = (int) (4 * Math.PI * r * r);
+        s = (int) (Math.PI * r * r);
+    }
+}
